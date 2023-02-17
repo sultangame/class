60db789f335527e68568dcb4ab8b73eeb9ddfaec
sultangame <sultankurbanov404@gmail.com>
2023-02-17 19:53:46 +0500

    hw

---
+++ class_hw.py
@@ -0,0 +1,26 @@
+"""class Reader(object):
+    def __init__(self,name, age, collage, number, num_of_lib):
+        self.name = name
+        self.age = age
+        self.collage = collage
+        self.number = number
+        self.num_of_lib = num_of_lib
+    def discription(self):
+        return f"{self.name} любит читать" \
+               f" Ему {self.age}  лет" \
+               f" он  из {self.collage} " \
+               f"его телефонный номер:{self.number}" \
+               f" Его читательский номер:{self.num_of_lib}"
+    def takeBook(self):
+        return f"{self.name} взлял три книги"
+    def returnBook(self):
+        return f"{self.name} вернул три книги"
+
+
+if __name__ =="__main__":
+        sultan = Reader("Султан", 13, "школа номер 12", "+998887879644", 120935)
+        print(sultan.discription())
+        print(sultan.takeBook())
+        print(sultan.returnBook())"""
+
+
